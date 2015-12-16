fda59e65f4426b844349467a406f70f8b2cd3aa6
jihyun-cho <cozyhyunny@gmail.com>
2015-12-16 13:57:52 +0900

    s

--- SKP_TMS/src/com/skp/tms/common/trance/GeoSystem.java
+++
@@ -1,9 +0,0 @@
-package com.skp.tms.common.trance;
-
-/**
- * Desc     : 좌표변환계 변수 설정 
- */
-public class GeoSystem { 
-    public final static int kGeographic = 0;
-    public final static int kKatec = 4;
-}
